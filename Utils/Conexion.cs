4cbb7355078831cac7b45a9c546272574f2e0b31
Yimy Herrera <yimyherrera97@gmail.com>
2025-05-30 00:46:21 -0500

    Se crea la clase conexion.cs para conectar con a la base datos sql server

---
+++ Utils/Conexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace FerreteriaApp.Data
+{
+    public class ConexionDB
+    {
+        private static SqlConnection _conexion;
+
+        private ConexionDB() { }
+
+        public static SqlConnection ObtenerConexion()
+        {
+            if (_conexion == null)
+            {
+                _conexion = new SqlConnection("Server=TU_SERVIDOR;Database=FerreteriaDB;Trusted_Connection=True;");
+            }
+
+            if (_conexion.State == System.Data.ConnectionState.Closed)
+                _conexion.Open();
+
+            return _conexion;
+        }
+    }
+}
