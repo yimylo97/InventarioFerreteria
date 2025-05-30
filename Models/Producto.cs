466895d7a099659ce7b6f419e64e8092fdcc62cb
Yimy Herrera <yimyherrera97@gmail.com>
2025-05-30 00:51:44 -0500

    Se crea clase Producto con sus propiedades

---
+++ Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerreteriaApp.Models
+{
+    public class Producto
+    {
+        public int ProductoID { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public decimal Precio { get; set; }
+        public int CantidadInventario { get; set; }
+    }
+}
+
