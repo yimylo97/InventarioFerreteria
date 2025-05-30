d45caaf2622294950fa968e00375487dd93cc7f3
Yimy Herrera <yimyherrera97@gmail.com>
2025-05-30 03:03:09 -0500

    Se crea la clase Usuario con sus propiedades

---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioFerreteria.Models
+{
+    public class Usuario
+    {
+        public int UsuarioID { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contrasena { get; set; }
+        public string Rol { get; set; }
+    }
+}
